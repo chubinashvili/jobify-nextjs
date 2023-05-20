3a17d12b70822f46dcb2f9b0794fa197d8e449f5
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-05-20 15:44:58 +0400

    feat: add useOnOutsideClick hook

---
+++ hooks/useOnOutsideClick.ts
@@ -0,0 +1,17 @@
+import { useEffect, RefObject } from 'react'
+
+export const useOnOutsideClick = (ref: RefObject<HTMLElement>, handler: (event: MouseEvent) => void) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler(event)
+      }
+    }
+
+    document.addEventListener('mousedown', listener)
+
+    return () => {
+      document.removeEventListener('mousedown', listener)
+    }
+  }, [ref, handler])
+}
