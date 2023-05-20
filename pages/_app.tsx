8cdb0f963d0eab4e9187296e194f88a43855f318
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-05-20 15:58:44 +0400

    refactor: wrap application with redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { wrapper } from '@/store'
+import '@/styles/globals.css'
+
+const MyApp: NextPage<AppProps> = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest)
+
+  return (
+    <>
+      <Head>
+        <title>Jobify</title>
+        <meta name='description' content='jobify' />
+        <meta name='referrer' content='no-referrer-when-downgrade' />
+        <meta property='og:site_name' content='jobify' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='jobify' />
+        <meta property='og:description' content='jobify' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='jobify-icon.ico' />
+      </Head>
+      <Provider store={store}>
+        {/* @ts-ignore */}
+        <PersistGate persistor={store.__persistor}>
+          <Component {...props.pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
+  )
 }
+
+export default MyApp
