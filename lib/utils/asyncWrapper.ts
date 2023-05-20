5dbdf88ec639d72466d5f5d611dc754c36811d12
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-05-20 07:30:33 +0400

    feat: add async wrapper for routes

---
+++ lib/utils/asyncWrapper.ts
@@ -0,0 +1,20 @@
+import { ServerError, BadRequestError, UnauthenticatedError, NotFoundError } from '../errors'
+
+type Controller = (...args: any[]) => Promise<void>
+
+const asyncWrapper = (controller: Controller) => {
+  return async (...args: any[]) => {
+    try {
+      await controller(...args)
+    } catch (error) {
+      if (error instanceof BadRequestError || error instanceof UnauthenticatedError || error instanceof NotFoundError) {
+        throw error
+      } else {
+        console.log(error)
+        throw new ServerError('Server Error')
+      }
+    }
+  }
+}
+
+export default asyncWrapper
